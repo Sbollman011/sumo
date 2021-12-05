af461689751db8b9e9649fdf210f551340707e14
Steven Bollman <steven.bollman@microfocus.com>
2021-12-04 17:49:57 -0800

    All the answers

---
+++ stevenTest.js
@@ -0,0 +1,35 @@
+
+
+function openNavBar () {
+   let menu =  document.getElementsByClassName('navDrawer')[0];
+   if(menu.style.visibility == 'hidden'){
+    menu.style.visibility = 'visible'
+    console.log(menu.style.visibility);
+   }
+   else{
+    menu.style.visibility = 'hidden'
+   }
+
+}
+
+
+function makeGetRequest() {
+    let datas;
+    fetch('http://localhost:3000/').then(function (response) {
+        // The API call was successful!
+        return response.json();
+    }).then(function (data) {
+        // This is the JSON from our response
+        console.log(data);
+        datas = JSON.stringify(data);
+        document.getElementById("response").innerHTML = datas;
+    }).catch(function (err) {
+        // There was an error
+        console.warn('Something went wrong.', err);
+    });
+    
+    }
+
+function clearApi() {
+    document.getElementById("response").innerHTML = "Info cleared";
+}
